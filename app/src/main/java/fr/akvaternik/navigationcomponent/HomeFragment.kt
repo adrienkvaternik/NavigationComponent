2540db4e6cae7d92afd8ba41987dbf2e62ef70be
Adrien Kvaternik <adrien.kvaternik@gmail.com>
2018-11-18 19:06:00 +0100

    Add button to go to details

--- app/src/main/java/fr/akvaternik/navigationcomponent/HomeFragment.kt
+++ app/src/main/java/fr/akvaternik/navigationcomponent/HomeFragment.kt
@@ -5,10 +5,20 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import kotlinx.android.synthetic.main.fragment_home.*
 
 class HomeFragment : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.fragment_home, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        button.setOnClickListener {
+            findNavController().navigate(R.id.go_to_details)
+        }
     }
 }
