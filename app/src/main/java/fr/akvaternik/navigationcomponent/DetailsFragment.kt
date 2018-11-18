7f986bd56292d683b77a6b77238f24e3b4cf96d4
Adrien Kvaternik <adrien.kvaternik@gmail.com>
2018-11-18 10:57:23 +0100

    Add details destination

---
+++ app/src/main/java/fr/akvaternik/navigationcomponent/DetailsFragment.kt
@@ -0,0 +1,14 @@
+package fr.akvaternik.navigationcomponent
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+class DetailsFragment : Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.fragment_details, container, false)
+    }
+}
