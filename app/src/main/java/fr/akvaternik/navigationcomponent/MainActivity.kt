cce6c0ce0d1e9744101f3449ec660d085be196ff
Adrien Kvaternik <adrien.kvaternik@gmail.com>
2018-11-19 09:59:00 +0100

    Bind toolbar to navigation

--- app/src/main/java/fr/akvaternik/navigationcomponent/MainActivity.kt
+++ app/src/main/java/fr/akvaternik/navigationcomponent/MainActivity.kt
@@ -1,12 +1,20 @@
 package fr.akvaternik.navigationcomponent
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    private val navController by lazy { findNavController(R.id.navHost) }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        setSupportActionBar(toolbar)
+        toolbar.setupWithNavController(navController, null)
     }
 }
